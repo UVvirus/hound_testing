bc97b999c19fd4e5be9fac69026d6701faa0fb06
UVvirus <39599249+UVvirus@users.noreply.github.com>
2023-05-10 11:06:47 +0530

    Update quill2.sol

--- quill2.sol
+++ quill2.sol
@@ -8,7 +8,7 @@ pragma solidity ^0.8.17;
 
 interface IUniswapV2Factory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
-
+    function test() external view returns(uint);
     function feeTo() external view returns (address);
     function feeToSetter() external view returns (address);
     function getPair(address tokenA, address tokenB) external view returns (address pair);
